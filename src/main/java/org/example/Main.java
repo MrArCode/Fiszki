fb506fc0c12cbd6e305edabcf5cf3ec9812ac6ea
Artur Cegiełka-Posłuszny <a.c.posluszny@gmail.com>
2024-04-27 15:34:47 +0200

    Adding new words to deck.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,36 +1,56 @@
 package org.example;
-
+import java.util.LinkedList;
 import java.util.Scanner;
-import java.util.SortedMap;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] polskieSłowa = new String[3];
-        String[] angielskieSłowa = new String[3];
+        LinkedList<Words> words = new LinkedList<>();
 
-        polskieSłowa[0] = "woda";
-        polskieSłowa[1] = "ogień";
-        polskieSłowa[2] = "ziemia";
+//        polskieSłowa[0] = "woda";
+//        polskieSłowa[1] = "ogień";
+//        polskieSłowa[2] = "ziemia";
+//
+//        angielskieSłowa[0] = "water";
+//        angielskieSłowa[1] = "fire";
+//        angielskieSłowa[2] = "earth";
 
-        angielskieSłowa[0] = "water";
-        angielskieSłowa[1] = "fire";
-        angielskieSłowa[2] = "earth";
+        System.out.println("Czy chesz dodac słowo y/n");
+        String czyKontynuwać = scanner.next();
+
+        while (czyKontynuwać.equals("y")){
+            System.out.println("Podaj polskie slowo: ");
+            String polishWord = scanner.next();
+
+            System.out.println("Podaj angielskie slowo: ");
+            String englishWord = scanner.next();
+
+            Words word = new Words(polishWord, englishWord);
+            words.add(word);
+
+            System.out.println("Czy chesz dodac słowo y/n");
+            czyKontynuwać = scanner.next();
+        }
+        System.out.println("Skończone");
+
+
+
+
 
         int counter = 0;
 
-        for (int i = 0; i < polskieSłowa.length; i++) {
-            System.out.println("Podaj slowo " + polskieSłowa[i] + " w języku angielskim: ");
+        for (Words word: words) {
+            System.out.println("Podaj slowo " + word.getPolishWord() + " w języku angielskim: ");
             String answer = scanner.next();
-            if (answer.equals(angielskieSłowa[i])) {
+            if (answer.equals(word.getEnglishWord())) {
                 System.out.println("Gratulacje, poprawna odpowiedz");
                 counter++;
             } else {
                 System.out.println("Błe∂na odpowiedz");
             }
-
         }
+
         System.out.println("Ostateczny wynik:" + counter);
     }
 }
